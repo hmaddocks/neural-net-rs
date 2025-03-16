59ba0e0326bdb92d614f01ed74a4987729c5ee67
Henry Maddocks <hmaddocks@me.com>
2025-03-16 16:04:43 +1300

    Disable rubocop

--- read_binary.rb
+++ read_binary.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/env ruby
 
 filename = File.expand_path("~/Documents/NMIST/train-images-idx3-ubyte")
-
+# rubocop: disable all
 begin
   File.open(filename, "rb") do |file|
     # Read and parse header (first 16 bytes)
     header = file.read(16).bytes
-    magic_number = header[0..3].pack('C*').unpack('N')[0]
-    num_images = header[4..7].pack('C*').unpack('N')[0]
-    num_rows = header[8..11].pack('C*').unpack('N')[0]
-    num_cols = header[12..15].pack('C*').unpack('N')[0]
+    magic_number = header[0..3].pack('C*').unpack1('N')
+    num_images = header[4..7].pack('C*').unpack1('N')
+    num_rows = header[8..11].pack('C*').unpack1('N')
+    num_cols = header[12..15].pack('C*').unpack1('N')
 
     puts "File Header Information:"
     puts "Magic Number: #{magic_number} (should be 2051 for images)"
@@ -17,7 +17,7 @@ begin
     puts "Number of Rows: #{num_rows}"
     puts "Number of Columns: #{num_cols}"
     puts "\nFirst 50 bytes of image data:"
-    
+
     # Read first 50 bytes of actual image data
     image_data = file.read(50).bytes
     image_data.each_with_index do |byte, index|
