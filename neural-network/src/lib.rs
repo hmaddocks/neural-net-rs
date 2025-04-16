b1fdc5ee274d56123e4aff0e06b725ea8babf030
Henry Maddocks <hmaddocks@me.com>
2025-04-17 11:55:33 +1200

    Clean up the exports in neural-network lib.rs

--- neural-network/src/lib.rs
+++ neural-network/src/lib.rs
@@ -1,10 +1,16 @@
-extern crate derive_builder;
-
+// Modules
 pub mod activations;
 pub mod layer;
-pub mod network;
-pub mod network_config;
-pub mod training_history;
 pub mod matrix {
     pub use matrix::matrix::Matrix;
 }
+pub mod network;
+pub mod network_config;
+pub mod training_history;
+
+pub use crate::activations::ActivationType;
+pub use crate::layer::Layer;
+pub use crate::matrix::Matrix;
+pub use crate::network::Network;
+pub use crate::network_config::NetworkConfig;
+pub use crate::training_history::TrainingHistory;
